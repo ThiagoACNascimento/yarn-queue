631c38264e601cef472b217f681a907a75d9f888
Thiago Costa <thiagoacnascimento@gmail.com>
2026-05-03 16:22:05 -0300

    chore: add AuthModule and UserModule to AppModule and omit user password in PrismaService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { HealthModule } from './modules/health/health.module';
 import { PrismaModule } from './infra/database/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
@@ -11,6 +13,8 @@ import { ConfigModule } from '@nestjs/config';
     }),
     HealthModule,
     PrismaModule,
+    AuthModule,
+    UsersModule,
   ],
 })
 export class AppModule {}
